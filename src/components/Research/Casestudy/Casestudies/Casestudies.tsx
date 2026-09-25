2798ab80d61d4c1c25e41e354b61aa6a473bc7f7
Prakash-r29 <p.prakash292002@gmail.com>
2026-09-26 00:02:26 +0530

    Fix CSS import case for casestudydetails

--- src/components/Research/Casestudy/Casestudies/Casestudies.tsx
+++ src/components/Research/Casestudy/Casestudies/Casestudies.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import "./CaseStudies.css";
+import "./Casestudies.css";
 
 interface CaseStudy {
   id: any;
