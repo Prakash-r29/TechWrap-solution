ba6527d6ba11d1b27af1a19b8973cbb16513e301
Prakash-r29 <p.prakash292002@gmail.com>
2026-09-25 23:57:15 +0530

    Fix CSS import case for Platformdetails

--- src/components/Capability/Platformdetails.tsx
+++ src/components/Capability/Platformdetails.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./PlatformDetails.css";
+import "./Platformdetails.css";
 
 interface PlatformSection {
   id: string;
